adf52238d8e354a10acf98ba3188dda6f545d867
Mehmet Karakuş <imeka4k@gmail.com>
2023-12-16 22:57:03 +0300

    add

---
+++ Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly HttpExceptionHandler _exceptionHandler;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+        _exceptionHandler = new HttpExceptionHandler();
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+
+            await HandleExceptionAsync(context.Response, exception);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpResponse response,Exception exception)
+    {
+        response.ContentType = "application/json";
+        _exceptionHandler.Response = response;
+        return _exceptionHandler.HandleExceptionAsync(exception);
+    }
+
+}
---
+++ Core.CrossCuttingConcerns/Exceptions/HttpProblemDetails/InternalServerErrorProblemDetails.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+
+public class InternalServerErrorProblemDetails : ProblemDetails
+{
+    public InternalServerErrorProblemDetails(string details)
+    {
+        Title = "Internal Error";
+        Detail = "Internal Error";
+        Status = StatusCodes.Status400BadRequest;
+        Type = "https://example.com/probs/business";
+    }
+}
+
